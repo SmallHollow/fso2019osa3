8a4e9205079250c090c76322ceca8caa693ff95f
Esa Onttonen <esa@onttonen.info>
2019-05-16 16:21:55 +0300

    3.1 puhelinluettelon backend step1

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+let phonebook = [
+  {
+      id: 1,
+      name: "Arto Hellas",
+      number: "045-1236543"
+  },
+  {
+      id: 2,
+      name: "Arto Järvinen",
+      number: "041-21423123"
+  },
+  {
+      id: 3,
+      name: "Lea Kutvonen",
+      number: "040-4323234"
+  },
+  {
+      id: 4,
+      name: "Martti Tienari",
+      number: "09-784232"
+  },
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Wörld!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(phonebook)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
