f822c9b13655def5b55cee35c18ed1ecfcbdf94f
Esa Onttonen <esa@onttonen.info>
2019-05-18 10:48:39 +0300

    uibuild

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fso2019osa3
 Full Stack Open 2019 - osa 3
+
+Linkki sovellukseen: (tulossa)
---
+++ build/precache-manifest.20557d45c3a34e343eb14954e9087074.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae363e7ea4e209e3ecd2871a936c3e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1c4be18f6bf11473547",
+    "url": "/static/css/main.2585dac2.chunk.css"
+  },
+  {
+    "revision": "37cfae212e25180f959b",
+    "url": "/static/js/2.0b74813f.chunk.js"
+  },
+  {
+    "revision": "b1c4be18f6bf11473547",
+    "url": "/static/js/main.0683ea69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
